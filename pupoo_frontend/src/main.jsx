320a4599a0a6efac186f2514d77d7663e98f76f8
Jaehyeok <babajae1@gmail.com>
2026-02-21 20:40:20 +0900

    feat(user-frontend): apply AuthProvider and header login state switching

--- pupoo_frontend/src/main.jsx
+++ pupoo_frontend/src/main.jsx
@@ -4,10 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
 
+import { AuthProvider } from "./pages/site/auth/AuthProvider"; 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
