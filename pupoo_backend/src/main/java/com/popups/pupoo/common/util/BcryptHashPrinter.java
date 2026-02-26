765fca394fe3fd7d8f9a3eb6e6a92e04d31a7372
[nkw421] <[https://github.com/nkw421/popups.git]>
2026-02-26 17:42:40 +0900

    fix

--- pupoo_backend/src/main/java/com/popups/pupoo/common/util/BcryptHashPrinter.java
+++ pupoo_backend/src/main/java/com/popups/pupoo/common/util/BcryptHashPrinter.java
@@ -6,6 +6,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BcryptHashPrinter {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("admin1234"));
+        System.out.println(encoder.encode("1234"));
     }
 }
